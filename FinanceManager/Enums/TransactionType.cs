1bf5cefebc60e46fd074c1ca92d892849858d25a
RamSen-IT <ramsen_it@proton.me>
2025-10-15 16:46:44 +0300

    feat: add Transaction model and TransactionType enum

---
+++ FinanceManager/Enums/TransactionType.cs
@@ -0,0 +1,8 @@
+
+namespace FinanceManager.Enums;
+
+public enum TransactionType
+{
+    Income,
+    Expense
+}
---
+++ FinanceManager/Models/Transaction.cs
@@ -0,0 +1,24 @@
+
+using System.ComponentModel.DataAnnotations;
+using FinanceManager.Enums;
+
+namespace FinanceManager.Models;
+
+public class Transaction
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(200)]
+    public string Description { get; set; }
+
+    [Required]
+    public decimal Amount { get; set; }
+
+    [Required]
+    public DateTime Date { get; set; }
+
+    [Required]
+    public TransactionType Type { get; set; }
+}
